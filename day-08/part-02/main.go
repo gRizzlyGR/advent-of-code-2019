6605a1bbd3b70271008ef7840a39b82d5d994425
gRizzlyGR <giusrizzi@gmail.com>
2019-12-08 18:33:42 +0100

    Added day 8 solutions

---
+++ day-08/part-02/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	print(render(scanner.Text(), 25, 6), 25, 6)
+}
+
+func render(imageData string, b, h int) []byte {
+	size := b * h
+	image := make([]byte, size)
+	for layout := 0; layout < len(imageData); layout += size {
+		for i := layout; i < size+layout; i++ {
+			if imageData[i] != '2' && image[i%size] == 0 {
+				image[i%size] = imageData[i]
+			}
+		}
+	}
+
+	return image
+}
+
+func print(buffer []byte, b, h int) {
+	for i := 0; i < h; i++ {
+		for j := 0; j < b; j++ {
+			switch buffer[i*b+j] {
+			case '0':
+				fmt.Print(" ")
+			case '1':
+				fmt.Print("█")
+			}
+		}
+		fmt.Println()
+	}
+}
